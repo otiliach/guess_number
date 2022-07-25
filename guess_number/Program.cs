208467a9aa155e3a000812002b3769b60530b668
Otilia Chelmuș <eotilia1@gmail.com>
2022-07-25 12:28:27 +0300

    random changed to 101, int nullable to int, modified loops

--- guess_number/Program.cs
+++ guess_number/Program.cs
@@ -12,22 +12,19 @@
             WriteGameName();
 
             Random rnd = new Random();
-            int rNumber = rnd.Next(0, 100);
-            int? guess = null;         
+            int rNumber = rnd.Next(0, 101);
+            int guess;         
            
             do
             {
                 guess = InputGuessNumber();
 
-
-                if (guess == rNumber)
-                    break;
-                else
-                    if (guess < rNumber)
+                 if (guess < rNumber)
                 {
                     Console.WriteLine("Your number was too small, try again.");
                 }
                 else
+                if (guess > rNumber)
                 {
                     Console.WriteLine("Your number was too big, try again.");
                 }
@@ -37,27 +34,28 @@
             Console.Write("You guessed! :) The answer was {0}.", rNumber);
         }
 
-        private static int? InputGuessNumber()
+        private static int InputGuessNumber()
         {
-            int? guess = null;
-            string inputStr;
+            int guess;
+            string? inputStr;
             Console.Write("Enter a number between 0 and 100: ");
 
-            while (guess == default)
-            {
-                inputStr = Console.ReadLine() ?? string.Empty;
-                guess = int.TryParse(inputStr, out int temp) ? temp : default(int?);
+            inputStr = Console.ReadLine();
 
-                if (guess == default)
-                    Console.Write("Invalid enter! You must enter a number: ");
+            while (!int.TryParse(inputStr, out guess))
+            {
+                Console.Write("Invalid enter! You must enter a number: ");
+                inputStr = Console.ReadLine();
             }
+            
             return guess;
         }
 
         private static void WriteGameName()
         {
-            string gameName = "***Guess the number***\n";
-            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (gameName.Length / 2)) + "}", gameName));
+            string gameName = "***Guess the number***";
+            Console.WriteLine(string.Format("{0," + ((Console.WindowWidth / 2) + (gameName.Length / 2)) + "}", gameName));
+            Console.WriteLine();
         }
     }
 }
